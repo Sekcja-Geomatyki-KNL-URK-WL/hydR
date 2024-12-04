2fb43303ba41e74bf1ab3a4ef584c174f7a8b145
Jakub Miszczyszyn <kuba.miszczyszyn@gmail.com>
2024-12-04 16:58:05 +0100

    nowy opis funkcji

--- R/define_projections.R
+++ R/define_projections.R
@@ -1,13 +1,18 @@
-#' Title
+#' Define Projections for Spatial Data
 #'
-#' @param x
+#' This function reads spatial data from a file and assigns a specified Coordinate Reference System (CRS).
 #'
-#' @return
+#' @param x Path to the input spatial data file (e.g., shapefile).
+#' @param y A valid  EPSG code that you want to assign to the spatial data.
+#'
+#' @return An sf object with the defined CRS.
 #' @export
 #'
 #' @examples
-define_projections=function(x,y){
-  i=sf::st_read(x)
-  sf::st_crs(x)=y
+#' define_projections("path/to/input/file.shp", 4326)
 
+define_projections = function(x, y) {
+  i = sf::st_read(x)
+  sf::st_crs(i) = y
+  return(i)
 }
